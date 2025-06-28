498c75c798e64beb0a869195a3496f567aec7e9a
varni1223 <varnibhimani64@gmail.com>
2025-06-28 11:00:12 +0530

    Add files via upload

---
+++ test-3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Animal {
+private:
+    string name;
+    string sound;
+
+public:
+    // Setter methods
+    void setName(string n) {
+        name = n;
+    }
+
+    void setSound(string s) {
+        sound = s;
+    }
+
+    // Getter methods
+    string getName() {
+        return name;
+    }
+
+    string getSound() {
+        return sound;
+    }
+
+    // Function to make sound (will be overridden)
+    void makeSound() {
+        cout << name << " makes a sound: " << sound << endl;
+    }
+};
+
+// Derived class Dog
+class Dog : public Animal {
+public:
+    void bark() {
+        setSound("Woof!");
+        cout << getName() << " says: " << getSound() << endl;
+    }
+};
+
+// Derived class Cat
+class Cat : public Animal {
+public:
+    void meow() {
+        setSound("Meow!");
+        cout << getName() << " says: " << getSound() << endl;
+    }
+};
+
+int main() {
+    // Create Dog object
+    Dog dog;
+    dog.setName("Buddy");
+    dog.bark();  // Abstraction: just calling bark()
+
+    // Create Cat object
+    Cat cat;
+    cat.setName("Whiskers");
+    cat.meow();  // Abstraction: just calling meow()
+
+    return 0;
+}
